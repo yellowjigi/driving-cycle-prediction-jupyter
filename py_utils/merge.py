d74e760b53d737c7c13821f27aa527d8d2f2118c
Hoyeon Hwang <hoyeonjigi17@gmail.com>
2021-05-20 00:29:25 +0900

    Removed truncating head part

--- py_utils/merge.py
+++ py_utils/merge.py
@@ -10,18 +10,10 @@ csvs = zip(sorted(pred_csvs), sorted(gt_csvs))
 
 for pair in csvs:
     df_pred = pd.read_csv(pair[0])
-    if 'ARIMA' in pair[0]:
-        df_pred = df_pred[5:]
-
     df_gt = pd.read_csv(pair[1])
-    if 'ARIMA' in pair[1]:
-        df_gt = df_gt[5:]
 
     df_merged = pd.concat([df_pred['Soc'], df_gt['Soc']], axis=1)
     df_merged.columns = ['predict', 'label']
-
-    if 'ARIMA' in pair[0]:
-        df_merged.reset_index(drop=True, inplace=True)
 
     dest_path_name = pair[0].replace('pred', 'merged', 1)
 
