de4783ea5787488b5eecfc6bbdaf3398c6ad3a9a
jigi <yellowjigi@gmail.com>
2021-05-19 18:09:26 +0900

    Added speed_filter util

--- py_utils/speed_filter.py
+++ py_utils/speed_filter.py
@@ -4,7 +4,7 @@ import sys
 
 import pandas as pd
 
-if sys.argv[1] == '*':
+if sys.argv[1][-1] == '*':
     csvs = glob(sys.argv[1])
 else:
     csvs = [sys.argv[1]]
@@ -12,10 +12,10 @@ else:
 for csv in csvs:
     if '.py' in csv:
         continue
-        
+
     if os.path.isdir(csv):
         continue
-        
+
     df = pd.read_csv(csv)
 
     df.drop(['Unnamed: 0'], axis=1, inplace=True)
